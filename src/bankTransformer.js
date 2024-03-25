7a278de804990dba33a1c5367f7b30e1661c63bf
tmadar <travismadar@gmail.com>
2024-03-24 18:57:32 -0700

    handle weird format from abs

--- src/bankTransformer.js
+++ src/bankTransformer.js
@@ -1,8 +1,8 @@
 import { getBankDescription, checkShouldSkip } from './transformerHelpers.js';
 
-const getCategory = (amount) => parseFloat(amount) < 0 ? 'Debit' : '';
+const getCategory = (amount) => amount < 0 ? 'Debit' : '';
 
-const getAmountLabel = (amount) => parseFloat(amount) < 0 ? Math.abs(amount) : `-${amount}`;
+const getAmountLabel = (amount) => amount < 0 ? Math.abs(amount).toFixed(2) : `-${amount}`;
 
 export const bankTransformer = (record) => {
     const shouldSkip = checkShouldSkip(record.Description);
@@ -11,10 +11,15 @@ export const bankTransformer = (record) => {
         return [];
     }
 
+    let formatNumber = parseFloat(record.Amount).toFixed(2);
+    
+    if (!formatNumber.includes('.')) {
+        formatNumber += ".00";
+    }
+
     const description = getBankDescription(record.Description);
-    const transactionCategory = getCategory(record.Amount);
-    const amountLabel = getAmountLabel(record.Amount);
-    console.log(parseFloat(record.Amount));
+    const transactionCategory = getCategory(formatNumber);
+    const amountLabel = getAmountLabel(formatNumber);
 
     return [description, amountLabel, record['Posting Date'], transactionCategory];
 }
--- src/creditTransformer.js
+++ src/creditTransformer.js
@@ -1,8 +1,8 @@
 import { getCreditDescription, checkShouldSkip } from './transformerHelpers.js';
 
-const getCategory = (amount) => parseFloat(amount) < 0 ? 'Credit' : '';
+const getCategory = (amount) => amount < 0 ? 'Credit' : '';
 
-const getAmountLabel = (amount) => parseFloat(amount) < 0 ? Math.abs(amount) : `-${amount}`;
+const getAmountLabel = (amount) => amount < 0 ? Math.abs(amount).toFixed(2) : `-${amount}`;
 
 export const creditTransformer = (record) => {
     const shouldSkip = checkShouldSkip(record.Description);
@@ -10,10 +10,17 @@ export const creditTransformer = (record) => {
     if(shouldSkip) {
         return [];
     }
+
     
+    let formatNumber = parseFloat(record.Amount).toFixed(2);
+        
+    if (!formatNumber.includes('.')) {
+        formatNumber += ".00";
+    }
+
     const description = getCreditDescription(record.Description);
-    const transactionCategory = getCategory(record.Amount);
-    const amountLabel = getAmountLabel(record.Amount);
+    const transactionCategory = getCategory(formatNumber);
+    const amountLabel = getAmountLabel(formatNumber);
 
     return [description, amountLabel, record['Transaction Date'], transactionCategory];
 }
